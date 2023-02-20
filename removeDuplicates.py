c5746039b04a8f217fb6726b2d3f48263e333d73
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2023-02-20 13:31:37 -0500

    Update removeDuplicates.py

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,7 +1,6 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/solutions/?languageTags=python3
 
-from typing import List
-
+# from typing import List
 
 # class Solution:
 #     def removeDuplicates(self, nums: List[int]) -> int:
@@ -12,17 +11,30 @@ from typing import List
 #                 i += 1
 #                 nums[i] = nums[j]
 #         return i
+# numbers = [1, 2, 3, 4, 5, 5, 5, 5]
+# print(removeDuplicates(numbers))
 
-def removeDuplicates(self, nums: List[int]) -> int:
+# def removeDuplicates(nums: List[int]) -> int:
+#   i = 0
+#   for j in range(1, len(nums)):
+#     if nums[j] != nums[i]:
+#       i += 1
+#       nums[i] = nums[j]
+#   return i
+  
+from typing import List
 
-  i = 0
-  for j in range(1, len(nums)):
-    if nums[j] != nums[i]:
-      i += 1
-      nums[i] = nums[j]
-  return i
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
 
-if __name__ == "__main__":
-    numbers = [1, 2, 3, 4, 5, 5, 5, 5]
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i
+numbers = [1, 2, 3, 4, 5, 5, 5, 5]
 
-    removeDuplicates(numbers)
+solution = Solution()
+result = solution.removeDuplicates(numbers)
+print(result)
