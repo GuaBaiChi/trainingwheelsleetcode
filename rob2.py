4d3d77ae4f6d7c5f2fb0c893a5c8e946ee2c5f81
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2023-03-13 09:21:50 -0400

    Update rob2.py

--- rob2.py
+++ rob2.py
@@ -4,12 +4,50 @@
 
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        return
+    def rob(self, nums: list[int]) -> int:
+        odd = 0
+        even = 0
+        numslength = len(nums)
 
+        if numslength % 2 == 0:
+            for i in range(1, len(nums[1:]), 2):
+                element = nums[i]
+                odd += element
+        else:
+            for i in range((1, len(nums), 2)):
+                element = nums[i]
+                even += element
+
+        if odd > even:
+            return odd
+        else:
+            return even
+
+
+# for i in range(1,11,2):
 
 # driver code
 solution = Solution()
-nums = [2, 3, 2]
+nums = [2, 3, 2, 4, 6]
 result = solution.rob(nums)
 print(result)
+
+
+# class Solution:
+#     def rob(self, nums: list[int]) -> int:
+#         countera1 = 0
+#         countera2 = 0
+
+#         for num in nums[:-1]:
+#             temp = countera1
+#             countera1 = max(countera2 + num, countera1)
+#             countera2 = temp
+
+#         counterb1 = 0
+#         counterb2 = 0
+#         for num in nums[1:]:
+#             temp = counterb1
+#             counterb1 = max(counterb2 + num, counterb1)
+#             counterb2 = temp
+
+#         return max(countera1, counterb1)
