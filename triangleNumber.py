f5ceecd2d4f87a82d37756cbf307866d64bf0f3a
guabaichi <leon.antonio.rodriguez.sanpere@gmail.com>
2023-03-28 13:33:53 -0400

    triangeNumber

--- triangleNumber.py
+++ triangleNumber.py
@@ -1,39 +1,33 @@
 # https://leetcode.com/problems/valid-triangle-number/
 # 611. Valid Triangle Number
+# https://www.youtube.com/watch?v=3Jo1NdD8xX4
+
+# i <= j < k
+# i + j > k
+
+# [2, 3, 4, 4]
+# [i, j, k, #]
 
 
 class Solution:
     def triangleNumber(self, nums: list[int]) -> int:
-        n = len(nums)
-        count = 0
         nums.sort()
-        for i in nums[-2:n]:
-            for j in nums[-1:n]:
-                for k in nums:
-                    if nums[i] + nums[j] > nums[k]:
-                        count += 1
-        return count
+        n = len(nums)
+        answer = 0
+
+        for k in range(2, n):
+            i, j = 0, k - 1
+            while i < j:
+                if nums[i] + nums[j] > nums[k]:
+                    answer += j - i
+                    j -= 1
+                else:
+                    i += 1
+
+        return answer
 
 
 solution = Solution()
 nums = [2, 2, 3, 4]
 result = solution.triangleNumber(nums)
 print(result)
-
-
-# class Solution:
-#     def triangleNumber(self, nums: list[int]) -> int:
-#         nums.sort()
-#         n = len(nums)
-#         answer = 0
-
-#         for k in range(2, n):
-#             i, j = 0, k-1
-#             while i < j:
-#                 if nums[i] + nums[j] > nums[k]:
-#                     answer += j - i
-#                     j -= 1
-#                 else:
-#                     i += 1
-
-#         return answer
